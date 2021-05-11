cd66440bf90e88a3efad3d19a286ed30560baaf9
Dominika Pawelska <dominikapawelska@dominikas-macbook-pro.home>
2021-05-11 11:56:07 +0200

    2021-05-11 DP

---
+++ app/models/product.py
@@ -0,0 +1,32 @@
+
+from app import app
+from app.utils import extractElement
+from app.models.opinion import Opinion
+import requests
+from bs4 import BeautifulSoup
+
+class Product:
+
+    url_pre = 'https://www.ceneo.pl'
+    url_post = '#tab=reviews'
+
+    def __init__(self, productId=None, name=None, opinions=[]):
+        self.productId = productId
+        self.name = name
+        self.opinions = opinions
+
+    def extractProduct(self):
+        url = self.url_pre+'/'+self.productId+self.url_post
+        while url:
+            respons = requests.get(url)
+            pageDOM = BeautifulSoup(respons.text, 'html.parser')
+            opinions = pageDOM.select("div.js_product-review")
+            for opinion in opinions:
+                self.opinions.append(Opinion().extractOpinion(opinion))
+            try:
+                url = self.url_pre + extractElement(pageDOM, 'a.pagination__next', "href") 
+            except TypeError:
+                url = None
+
+    def __str__(self):
+        pass
